535cfc99e58e4e6c8a43c251ffc0deef985a964b
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-02 13:55:31 +0300

    Css edited

--- code/src/App.js
+++ code/src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import SignIn from './pages/SignIn/SignIn';
-import Home  from './pages/Home/Home';
-import Messages from './pages/Messages/Messages';
-import ErrorPage from './pages/ErrorPage';
-import Admin from './pages/Admin/Admin';
-import Cat from './pages/Cat/Cat';
+import  Nav from './components/Nav/Nav'
 
 function App() {
   return (
-    <Router>
-    <nav>
-
-  
-   <Link to='/home'> Home </Link>
-   <Link to='/'> SignIn </Link>
-   <Link to='/admin'> Admin</Link>
-   <Link to='/cat' > Cat </Link>
-   <Link to='/message'> Message </Link>
-
-    </nav>
-    <Routes> 
-    {/* Used instead of switch */}
-    <Route path='/' element={<SignIn/>}/>
-    {/* main page of the route */}
-    <Route path='/home' element={<Home/>}/>
-    <Route path='/admin' element={<Admin/>}/>
-    <Route path='/cat' element={<Cat/>}/>
-    <Route path='/message' element={<Messages/>}/>
-    <Route path ='*' element={<ErrorPage/>}/>
-    
-    </Routes>
-    </Router>
+    <div className='App'>
+      <Nav/>
+    </div>
   );
 }
 
--- code/src/components/Nav/Nav.js
+++ code/src/components/Nav/Nav.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import SignIn from '../../pages/SignIn/SignIn';
+import Home  from '../../pages/Home/Home';
+import Messages from '../../pages/Messages/Messages';
+import ErrorPage from '../../pages/ErrorPage';
+import Admin from '../../pages/Admin/Admin';
+import Cat from '../../pages/Cat/Cat';
 
-function Nav(){
-return(
-    <div className="navbar">
-        <h2>Navigation Pane</h2>
-        
+function Nav() {
+  return (
+    <Router>
 
-    </div> 
-)
+    <nav style={
+    {
+    background: 'white', 
+    height: 35, 
+    fontSize :17, 
+    fontWeight: 'bold', 
+    alignContent: 'center'
+    }}>
+
+   <Link to='/home'> Home </Link>
+   <Link to='/'> SignIn </Link>
+   <Link to='/admin'> Admin</Link>
+   <Link to='/cat' > Cat </Link>
+   <Link to='/message'> Message </Link>
+
+    </nav>
+    <Routes> 
+    {/* Used instead of switch */}
+    <Route path='/' element={<SignIn/>}/>
+    {/* main page of the route */}
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/admin' element={<Admin/>}/>
+    <Route path='/cat' element={<Cat/>}/>
+    <Route path='/message' element={<Messages/>}/>
+    <Route path ='*' element={<ErrorPage/>}/>
+    
+    </Routes>
+    </Router>
+  );
 }
-export  default Nav;
+
+export default Nav;
